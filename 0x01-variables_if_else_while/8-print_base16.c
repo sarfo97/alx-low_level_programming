bd26c19cdddb5f210880799469ba2d1c15d76ba3
Sarfo97 <owilliams001@st.ug.edu.gh>
2024-06-21 21:58:28 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n < 103; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
