ceee5e04447916df230fdf576f7023bb4fd0bd93
Sarfo97 <owilliams001@st.ug.edu.gh>
2024-06-21 22:05:13 -0700

    print combination 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48; a < 56; a++)
+	{
+		for (b = a + 1; b < 57; b++)
+		{
+			for (c = b + 1; c < 58; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+				{
+					putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
