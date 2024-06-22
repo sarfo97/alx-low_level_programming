3e882c32e7e8872218964a14d951e7c90b7ac22f
Sarfo97 <owilliams001@st.ug.edu.gh>
2024-06-21 22:07:20 -0700

    print combination 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = a + 1; b < 100; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				putchar('\n');
+			}
+		}
+	}
+	return (0);
+}
