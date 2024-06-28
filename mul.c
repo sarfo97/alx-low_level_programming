c19e79dcd576384c1b158efe05a364677c5d7f5a
Sarfo97 <owilliams001@st.ug.edu.gh>
2024-06-27 22:16:57 -0700

    multiply

---
+++ mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - multiples two numbers
+ *
+ * @a: integer number
+ * @b: integrer number
+ *
+ * Return: returns the multiplication result
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
