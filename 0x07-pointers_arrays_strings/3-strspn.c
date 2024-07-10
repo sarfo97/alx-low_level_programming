3815b0c541c464909598cad24751c16af874fe06
Sarfo97 <owilliams001@st.ug.edu.gh>
2024-07-09 22:49:33 -0700

    hmmn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,11 @@
-#include "main.h"
-
+#include <string.h>
 /**
-  * _strspn - search a string for a set of bytes
-  * @s: source string
-  * @accept: accepted string
-  * Return: number of bytes in the init segment
-  */
+ * _strspn - length if prefix
+ * @s: styring
+ * @accept: string
+ * Return: unsigned int
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j, a = 0;
-
-	while (accept[i)
-	{
-		j = 0;
-
-		while (s[j] != 32)
-		{
-			if (accept[i] == s[j])
-			{
-				a++;
-			}
-
-			j++;
-		}
-
-		i++;
-	}
-
-	return (a);
+	return (strspn(s, accept));
 }
